64e5ff92c9fc73f1259fa8d2a59f5287db5167d8
Georgie Conway <dopestantelope@gmail.com>
2022-06-27 20:13:06 +0100

    a lot of changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import * as s from './selectorvariables.js'
+
 let deckId = ''
 getFetch()
 function getFetch(){
@@ -14,15 +16,16 @@ function getFetch(){
       });
 }
 
-document.querySelector('button').addEventListener('click', drawCard)
+document.querySelector('button').addEventListener('click', deal)
 
-function drawCard(){
+function deal(){
   const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        reset()
         boardInit(data)
       })
       .catch(err => {
@@ -31,15 +34,23 @@ function drawCard(){
 
 }
 
+function reset() {
+  s.playerFirstCard.src = ''
+  s.dealerFirstCard.src = ''
+  s.playerSecondCard.src = ''
+  s.dealerSecondCard.src = ''
+  s.dealerSecondCard.style.display = ''
+}
+
 function boardInit(data) {
   // deals the cards with a dealer card face down
-  document.querySelector('h2').style.display = 'none'
-  document.querySelector('.title-screen').style.display = 'none'
-  document.querySelector('.player-first').src = data.cards[0].image
-  document.querySelector('.dealer-first').src = data.cards[1].image
-  document.querySelector('.player-second').src = data.cards[2].image
-  document.querySelector('.dealer-second').src = "img/back-card.jpeg"
-  document.querySelector('.dealer-second').style.display = 'inline-block'
+  s.result.style.display = 'none'
+  s.titleScreen.style.display = 'none'
+  s.playerFirstCard.src = data.cards[0].image
+  s.dealerFirstCard.src = data.cards[1].image
+  s.playerSecondCard.src = data.cards[2].image
+  s.dealerSecondCard.src = "img/back-card.jpeg"
+  s.dealerSecondCard.style.display = 'inline-block'
 
   let p1 = convertToNum((data.cards[0].value))
   let p2 = convertToNum((data.cards[2].value))
@@ -48,21 +59,62 @@ function boardInit(data) {
   let playerTotal = p1 + p2
   let dealerTotal = d1
 
-  score(playerTotal, dealerTotal)
+  playerScore(playerTotal)
+  dealerScore(dealerTotal)
+
+  s.hitButton.addEventListener('click', hitMe)
+
+  function hitMe() {
+    const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+  
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data)
+          const newCard = document.createElement('img')
+          newCard.src = data.cards[0].image
+          document.querySelector('.player-cards').appendChild(newCard)
+          playerTotal += convertToNum(data.cards[0].value)
+          playerScore(playerTotal)
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  
+  }
+
+
+
 
   // stand
-  document.getElementById('stand').onclick = function() {
-    document.querySelector('.dealer-second').src = data.cards[3].image
-    document.querySelector('h2').style.display = 'inline-block'
-    document.querySelector('h2').innerText = `Dealer Wins!`
+  document.getElementById('stand').addEventListener('click', stand)
+  
+  function stand() {
+    s.dealerSecondCard.src = data.cards[3].image
+    s.result.style.display = 'inline-block'
+    dealerTotal += convertToNum(data.cards[3].value)
+    dealerScore(dealerTotal)
+    s.result.innerText = `Dealer Wins!`
 
   }
 
-  function score(playerTotal, dealerTotal) {
-    document.querySelector('.dealer-count').style.display = 'flex'
-    document.querySelector('.player-count').style.display = 'flex'
-    document.querySelector('.dealer-count').innerText = dealerTotal
-    document.querySelector('.player-count').innerText = playerTotal
+  function playerScore (playerCount) {
+    s.playerCount.style.display = 'flex'
+    s.playerCount.innerText = playerTotal
+    if (playerCount > 21) {
+      bust()
+    }
+  }
+
+  function dealerScore (dealerCount) {
+    s.dealerCount.style.display = 'flex'
+    s.dealerCount.innerText = dealerTotal
+  }
+
+  function bust() {
+    s.result.style.display = 'inline-block'
+    s.result.innerText = 'Bust!'
   }
 
 }
---
+++ js/selectorvariables.js
@@ -0,0 +1,20 @@
+export const titleScreen = document.querySelector('.title-screen');
+
+
+// Cards
+export const playerFirstCard = document.querySelector('.player-first')
+export const playerSecondCard = document.querySelector('.player-second')
+export const dealerFirstCard = document.querySelector('.dealer-first')
+export const dealerSecondCard = document.querySelector('.dealer-second')
+
+
+// Text
+export const result = document.querySelector('.winner')
+
+// Score
+export const dealerCount = document.querySelector('.dealer-count')
+export const playerCount = document.querySelector('.player-count')
+
+
+// Buttons
+export const hitButton = document.querySelector('#hit')
